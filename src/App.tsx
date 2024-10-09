10ed034bab44fea3d5b5435a6c736d1f3a2bb1f3
Chris Hartgerink <chris@libscie.org>
2024-10-09 15:52:47 +0200

    Update display cols

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import Grid from './Grid';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-    
+    <div className="md:flex flex-col min-h-screen">
+      <header className="grow-0 items-center text-center">
+        <h1>Find your next epidemiology tools</h1>
       </header>
       <Grid first={<div>Column/Row 1</div>} second={<div>Column/Row 2</div>} />
     </div>
--- src/Grid.tsx
+++ src/Grid.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ResponsiveGrid = ({first, second}: any) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 min-h-screen">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 grow">
       <div className="bg-blue-500 p-4">{first}</div>
       <div className="bg-green-500 p-4">{second}</div>
     </div>
