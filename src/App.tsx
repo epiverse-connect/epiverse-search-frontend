46528ecaf576586be806fab560047e6c6de7970c
Chris Hartgerink <chris@libscie.org>
2024-10-09 15:47:39 +0200

    Cleanup `create-react-app`, add Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Grid from './Grid';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='bg-red-500'>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
+      <Grid first={<div>Column/Row 1</div>} second={<div>Column/Row 2</div>} />
     </div>
   );
 }
---
+++ src/Grid.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ResponsiveGrid = ({first, second}: any) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 min-h-screen">
+      <div className="bg-blue-500 p-4">{first}</div>
+      <div className="bg-green-500 p-4">{second}</div>
+    </div>
+  );
+}
+
+export default ResponsiveGrid;
