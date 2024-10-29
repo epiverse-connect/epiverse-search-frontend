c92ad92fdbc478f34e7e4bd438967f997f20744b
Chris Hartgerink <chris@libscie.org>
2024-10-29 10:18:56 +0100

    Update alt texts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil';
+import { RecoilRoot } from 'recoil';
 import Grid from './Grid';
 import MapCol from './MapCol';
 import SearchCol from './SearchCol';
 import React from 'react';
-import LogoDataDotOrg from '../public/LogoDataDotOrg.png';
 import GitHubCorner from './GitHubCorner';
 
 function App() {
@@ -44,15 +37,14 @@ const LogoCloud = () => {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none">
           <img
-            alt="Transistor"
-            // src="https://tailwindui.com/plus/img/logos/158x48/transistor-logo-gray-900.svg"
+            alt="Data Dot Org"
             src="https://github.com/epiverse-trace/epiverse-trace.github.io/blob/main/public/LogoDataDotOrg.png?raw=true"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
           />
           <img
-            alt="Reform"
+            alt="World Health Organization"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/World_Health_Organization_Logo.svg/1024px-World_Health_Organization_Logo.svg.png"
             width={158}
             height={48}
