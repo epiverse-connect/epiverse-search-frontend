71173277b6fd45eb085bd8bebd862d55769aa5a5
Chris Hartgerink <chris@libscie.org>
2024-11-25 15:58:18 +0100

    Disable search until at least one whitespace

--- src/SearchCol.tsx
+++ src/SearchCol.tsx
@@ -24,8 +24,10 @@ const SearchBar = () => {
       );
       const data = await response.json();
       console.log(data);
-      setSearchResults(data);
-      setShowSearchResults(true);
+      if (!data.error) {
+        setSearchResults(data);
+        setShowSearchResults(true);
+      }
     } else {
       alert(
         'No backend URL found. Please set REACT_APP_BACKEND in your .env file.'
@@ -86,7 +88,7 @@ const SearchBar = () => {
               // setSearchResults([]);
             }}
             className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-35"
-            disabled={!searchQuery}
+            disabled={!searchQuery || !/\s/.test(searchQuery)}
           >
             Search
           </button>
