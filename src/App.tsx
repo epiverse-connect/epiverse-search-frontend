813214be38d236eb3737ddf86c6efb0ba6f21578
Chris Hartgerink <chris@libscie.org>
2025-04-22 17:39:28 +0200

    Update design to Collaboratory draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import SearchCol from './SearchCol';
 import React from 'react';
-import GitHubCorner from './GitHubCorner';
+import Header from './Header';
 
 function App() {
   return (
     <>
-      <div className="md:flex flex-col dark:bg-gray-900 dark:text-gray-50">
-        <header className="grow-0 items-center text-center my-16">
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-            Epiverse{' '}
-            <span className="underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">
-              Tool Search
-            </span>
-          </h1>
-          <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 m-2 lg:m-8">
-            Please describe the epidemiological task you would like the tools
-            for.
-          </p>
-        </header>
-        <SearchCol />
+      <div className="md:flex flex-col dark:bg-gray-900 dark:text-gray-50 p-4">
+        <Header />
+        <body className="w-full">
+          <SearchCol />
+        </body>
       </div>
-      <GitHubCorner />
+      {/* <GitHubCorner /> */}
     </>
   );
 }
--- src/SearchCol.tsx
+++ src/SearchCol.tsx
@@ -4,6 +4,7 @@ import exampleSearchData from '../src/example-search.json';
 const SearchCol = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [results, setResults] = useState({});
+  const [first, setFirst] = useState(true);
 
   const handleSearch = async () => {
     if (process.env.REACT_APP_BACKEND) {
@@ -14,6 +15,7 @@ const SearchCol = () => {
 
       if (!data.error) {
         setResults(data);
+        setFirst(false);
       }
     } else {
       alert(
@@ -23,40 +25,17 @@ const SearchCol = () => {
   };
 
   return (
-    <div>
-      <form className="max-w-md mx-auto">
-        {/* <h2 className="text-xl font-bold text-center my-4">
-          Type your task here
-        </h2> */}
-        <label
-          htmlFor="default-search"
-          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        >
-          Search
+    <>
+      <form className="w-[90%] mx-auto mt-16 text-lg">
+        <label htmlFor="default-search" className="sr-only">
+          Search bar
         </label>
         <div className="relative">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-          </div>
           <input
             type="search"
             id="default-search"
-            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Epidemiological task here..."
+            className="block w-full p-4 text-gray-900 border border-black focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Search..."
             value={searchQuery}
             onChange={(e) => {
               setSearchQuery(e.target.value);
@@ -72,29 +51,46 @@ const SearchCol = () => {
               e.preventDefault();
               setResults({});
             }}
-            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-35"
+            className="button absolute h-[62px] end-0 bottom-0 bg-[#00205C] disabled:opacity-35"
             disabled={!searchQuery}
           >
-            Search
+            <svg
+              className="w-8 h-8 text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
           </button>
         </div>
-        <SearchResults results={results} />
+        <SearchResults results={results} first={first} />
       </form>
-      <button
-        onClick={() => {
-          if (JSON.stringify(results) == '{}') {
-            setResults(exampleSearchData);
-          } else {
-            setResults({});
-          }
-        }}
-        className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-35"
-      >
-        {JSON.stringify(results) == '{}'
-          ? 'Set example results'
-          : 'Remove example results'}
-      </button>
-    </div>
+      <div className="static bottom-0 right-0 text-center">
+        <button
+          onClick={() => {
+            if (JSON.stringify(results) == '{}') {
+              setResults(exampleSearchData);
+              setFirst(false);
+            } else {
+              setResults({});
+            }
+          }}
+          className="button mt-16 end-2.5 bottom-2.5 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 disabled:opacity-35"
+        >
+          {JSON.stringify(results) == '{}'
+            ? 'Set example results'
+            : 'Remove example results'}
+        </button>
+      </div>
+    </>
   );
 };
 
@@ -103,76 +99,133 @@ export default SearchCol;
 interface SearchResultsProps {
   // eslint-disable-next-line
   results: any;
+  first: boolean;
 }
 
-const SearchResults: React.FC<SearchResultsProps> = ({ results }) => {
+const SearchResults: React.FC<SearchResultsProps> = ({ results, first }) => {
   if (JSON.stringify(results) != '{}') {
     return (
-      <div className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700">
-        <ul
-          className="py-2 text-sm text-gray-700 dark:text-gray-200"
-          aria-labelledby="dropdown-button"
-        >
-          {results.response.results.slice(0, 5).map(
-            (
-              result: {
-                package: string;
-                website: string;
-                source: string;
-                relevance: number;
-                logo: (string | undefined)[];
-              },
-              index: React.Key | null | undefined
-            ) => (
-              <SearchResult
-                key={index}
-                title={result.package}
-                url={result.website[0] || result.source}
-                score={result.relevance}
-                logo={result.logo[0]}
-              />
-            )
-          )}
-        </ul>
+      <div className="py-8 sm:px-4">
+        <h2 className="text-lg">
+          Search results 1-{results.response.results.length}
+        </h2>
+        <div className="z-10 divide-y divide-gray-100 sm:-mx-16 my-4">
+          <ul aria-labelledby="dropdown-button">
+            {results.response.results.slice(0, 5).map(
+              (
+                result: {
+                  package: string;
+                  title: string;
+                  description: string;
+                  logo?: string;
+                  website?: string;
+                  source?: string;
+                  vignettes?: string[];
+                },
+                index: React.Key | null | undefined
+              ) => (
+                <SearchResult
+                  key={index}
+                  pkg={result.package}
+                  title={result.title}
+                  description={result.description}
+                  logo={result.logo}
+                  url={result.website}
+                  source={result.source}
+                  vignettes={result.vignettes}
+                />
+              )
+            )}
+          </ul>
+        </div>
       </div>
     );
   } else {
-    return <></>;
+    if (first) {
+      return <></>;
+    } else {
+      return (
+        <div className="py-8 px-4">
+          <h2 className="text-lg">
+            Sorry, your search has not produced any results. Try changing your
+            search terms
+          </h2>
+        </div>
+      );
+    }
   }
 };
 
 const SearchResult = ({
+  pkg,
   title,
-  url,
+  description,
   logo,
-  score,
+  url,
+  source,
+  vignettes,
 }: {
+  pkg: string;
   title: string;
-  url: string;
+  description: string;
   logo?: string;
-  score: number;
+  url?: string;
+  source?: string;
+  vignettes?: string[];
 }) => {
+  const score = Math.random();
+
   return (
-    <li>
+    <li className="mx-auto">
       <a
-        href={url}
+        href={url || source}
         target="_blank"
         rel="noreferrer"
         className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
       >
+        <span className="text-sm">
+          <div className="w-32 bg-gray-200 rounded-full h-1.5 mb-2 dark:bg-gray-700">
+            <div
+              className="bg-[#F4A81D] h-1.5 rounded-full dark:bg-blue-500"
+              style={{ width: `${score * 100}%` }}
+            ></div>
+          </div>
+          <p className="font-semibold">
+            {pkg}: {title}
+          </p>
+          <p className="font-normal">{description}</p>
+          <p className="pt-1 pb-4">
+            <a href={url} className="mx-4 underline">
+              Website
+            </a>
+
+            <a href={source} className="mx-4 underline">
+              Source
+            </a>
+
+            <a href={`${source}/issues`} className="mx-4 underline">
+              Bug Tracker
+            </a>
+
+            {vignettes && (
+              <span className="mx-4">
+                Vignette{' '}
+                {vignettes.map((vignette, index) => (
+                  <a href={vignette} key={vignette} className="underline mx-1">
+                    {index + 1}
+                  </a>
+                ))}
+              </span>
+            )}
+          </p>
+        </span>
         {logo && (
           <img
             src={logo}
-            alt={`Logo of ${title}`}
-            className="w-auto h-max-4 h-4 mx-2"
+            alt={`Logo of ${title} package`}
+            className="h-max-4 w-[80px] mx-2"
           />
         )}
-        {title}
-        <span className="grow"></span>
-        {/* <code className="ml-2 text-xs text-gray-400">{filename}</code> */}
-        <span className="inline-flex items-center rounded-full bg-gray-100 px-1.5 py-0.5 text-xs font-medium text-gray-600">
-          {score.toString().slice(0, 5)}
-        </span>
       </a>
     </li>
   );
